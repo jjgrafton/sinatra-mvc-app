dfca0ef4b22e6fc8735a7ed4089cf1ffc41535d0
Jamie Grafton <jamiegrafton@gmail.com>
2018-04-23 16:15:11 -0400

    Added alternative methods into app.rb

--- app.rb
+++ app.rb
@@ -8,3 +8,18 @@ get "/robots" do
     @all_robots = Robot.all
     erb :index
 end
+
+get "/robots" do
+    Robot.find(1).destroy
+    @all_robots = Robot.all
+
+end
+
+get "/robots" do
+    Robot.where("angry" = "true"). destroy_all
+    get "/robots" do
+        Robot.find(1).destroy
+        @all_robots = Robot.all
+    
+    end
+end
